cbbb1f9f7c0e9f0e1b65891e3c35efda01af517f
FELIPE TAVARES BRAUN <1051392511022@fatec.corp>
2026-08-31 09:43:20 -0300

    Feat: Formula IMC

--- App.py
+++ App.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def home():
     return 'Aplicação funcionando!'
 
+@app.route("/status")
+def home():
+    imprimir("Olá, mundo!")
+    return "Olá Mundo"
+
+
 if __name__ == '__main__':
     print('Iniciando servidor...')
     app.run(host='127.0.0.1', port=5000, debug=True)
